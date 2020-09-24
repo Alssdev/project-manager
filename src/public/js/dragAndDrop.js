f45fcb4b4dc60efb16ba7f04383fef56997ab46a
alssdev <alssdev3@gmail.com>
2020-09-24 09:40:50 -0600

    drag and drop implementation

---
+++ src/public/js/dragAndDrop.js
@@ -0,0 +1,33 @@
+const toDo = document.getElementById('toDo')
+const inProgress = document.getElementById('inProgress')
+const completed = document.getElementById('completed')
+
+Sortable.create(toDo, {
+	group: {
+		name: 'tasks',
+	},
+	animation: 300,
+	chosenClass: 'task-selected',
+	dragClass: 'task-drag',
+	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+})
+
+Sortable.create(inProgress, {
+	group: {
+		name: 'tasks',
+	},
+	animation: 300,
+	chosenClass: 'task-selected',
+	dragClass: 'task-drag',
+	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+})
+
+Sortable.create(completed, {
+	group: {
+		name: 'tasks',
+	},
+	animation: 300,
+	chosenClass: 'task-selected',
+	dragClass: 'task-drag',
+	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+})
