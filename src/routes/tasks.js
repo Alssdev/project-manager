f9423947f700dc9493be0dca1e92a9a04b87e245
alssdev <alssdev3@gmail.com>
2020-09-25 11:31:25 -0600

    Delete task

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -20,6 +20,18 @@ router.get('/add', (req, res) => {
 	res.render('tasks/add');
 });
 
+router.get('/delete/:id', (req, res) => {
+	const { id } = req.params;
+
+	db.run(`DELETE FROM tasks WHERE id=${id}`, (err) => {
+		if (err) {
+			console.log(err.message);
+		}
+
+		res.redirect('/tasks');
+	});
+});
+
 router.post('/', (req, res) => {
 	const task = req.body;
 
