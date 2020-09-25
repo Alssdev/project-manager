8f55ed3938e14c20fa0e1c6dd6121a7a5cdc0e32
alssdev <alssdev3@gmail.com>
2020-09-25 16:07:54 -0600

    Implementation of local storage to keep the order of the cards

--- src/public/js/dragAndDrop.js
+++ src/public/js/dragAndDrop.js
@@ -11,7 +11,15 @@ Sortable.create(toDo, {
 	dragClass: 'task-drag',
 	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
 	store: {
-		set: function () {},
+		set: function (sortable) {
+			const sequence = sortable.toArray();
+			localStorage.setItem('sequence-toDo', sequence.join(','));
+		},
+
+		get: function (sortable) {
+			const sequence = localStorage.getItem('sequence-toDo').split(',');
+			return sequence.length ? sequence : [];
+		},
 	},
 });
 
@@ -23,6 +31,17 @@ Sortable.create(inProgress, {
 	chosenClass: 'task-selected',
 	dragClass: 'task-drag',
 	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+	store: {
+		set: function (sortable) {
+			const sequence = sortable.toArray();
+			localStorage.setItem('sequence-inProgress', sequence.join(','));
+		},
+
+		get: function (sortable) {
+			const sequence = localStorage.getItem('sequence-inProgress').split(',');
+			return sequence.length ? sequence : [];
+		},
+	},
 });
 
 Sortable.create(completed, {
@@ -33,4 +52,15 @@ Sortable.create(completed, {
 	chosenClass: 'task-selected',
 	dragClass: 'task-drag',
 	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+	store: {
+		set: function (sortable) {
+			const sequence = sortable.toArray();
+			localStorage.setItem('sequence-completed', sequence.join(','));
+		},
+
+		get: function (sortable) {
+			const sequence = localStorage.getItem('sequence-completed').split(',');
+			return sequence.length ? sequence : [];
+		},
+	},
 });
