7a6f96314a9d5f8179a40c886f920b71e9f1c917
alssdev <alssdev3@gmail.com>
2020-09-28 21:26:43 +0200

    Redirection to tasks, route /

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ app.use((req, res, next) => {
 });
 
 // routes
+app.use(require('./routes/index'));
 app.use(require('./routes/auth'));
 app.use('/tasks', require('./routes/tasks'));
 app.use('/dividers', require('./routes/dividers'));
