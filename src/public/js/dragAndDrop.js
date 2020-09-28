3c85fa44c27d9390a5c0fe95478501029d47ba71
alssdev <alssdev3@gmail.com>
2020-09-28 17:56:55 +0200

    Creation of the layout of the dividers

--- src/public/js/dragAndDrop.js
+++ src/public/js/dragAndDrop.js
@@ -3,91 +3,94 @@ const inProgress = document.getElementById('inProgress');
 const completed = document.getElementById('completed');
 
 Sortable.create(toDo, {
-	group: {
-		name: 'tasks',
-	},
-	animation: 300,
-	chosenClass: 'task-selected',
-	dragClass: 'task-drag',
-	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
-	store: {
-		set: function (sortable) {
-			const sequence = sortable.toArray();
-			localStorage.setItem('sequence-toDo', sequence.join(','));
-		},
+  group: {
+    name: 'tasks',
+  },
+  animation: 300,
+  chosenClass: 'task-selected',
+  dragClass: 'task-drag',
+  easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+  filter: '#divider',
+  store: {
+    set: function (sortable) {
+      const sequence = sortable.toArray();
+      localStorage.setItem('sequence-toDo', sequence.join(','));
+    },
 
-		get: function (sortable) {
-			let sequence = localStorage.getItem('sequence-toDo')
-			sequence = sequence ? sequence.split(',') : {length: 0};
+    get: function (sortable) {
+      let sequence = localStorage.getItem('sequence-toDo');
+      sequence = sequence ? sequence.split(',') : { length: 0 };
 
-			return sequence.length ? sequence : [];
-		},
-	},
+      return sequence.length ? sequence : [];
+    },
+  },
 
-	// save progress
-	onAdd: (event) => {
-		const id = event.item.attributes['data-id'].nodeValue;
+  // save progress
+  onAdd: (event) => {
+    const id = event.item.attributes['data-id'].nodeValue;
 
-		axios.put(`/tasks/${id}/state/1`);
-	},
+    axios.put(`/tasks/${id}/state/1`);
+  },
 });
 
 Sortable.create(inProgress, {
-	group: {
-		name: 'tasks',
-	},
-	animation: 300,
-	chosenClass: 'task-selected',
-	dragClass: 'task-drag',
-	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
-	store: {
-		set: function (sortable) {
-			const sequence = sortable.toArray();
-			localStorage.setItem('sequence-inProgress', sequence.join(','));
-		},
+  group: {
+    name: 'tasks',
+  },
+  animation: 300,
+  chosenClass: 'task-selected',
+  dragClass: 'task-drag',
+  easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+  filter: '#divider',
+  store: {
+    set: function (sortable) {
+      const sequence = sortable.toArray();
+      localStorage.setItem('sequence-inProgress', sequence.join(','));
+    },
 
-		get: function (sortable) {
-			let sequence = localStorage.getItem('sequence-toDo')
-			sequence = sequence ? sequence.split(',') : {length: 0};
+    get: function (sortable) {
+      let sequence = localStorage.getItem('sequence-toDo');
+      sequence = sequence ? sequence.split(',') : { length: 0 };
 
-			return sequence.length ? sequence : [];
-		},
-	},
+      return sequence.length ? sequence : [];
+    },
+  },
 
-	// save progress
-	onAdd: (event) => {
-		const id = event.item.attributes['data-id'].nodeValue;
+  // save progress
+  onAdd: (event) => {
+    const id = event.item.attributes['data-id'].nodeValue;
 
-		axios.put(`/tasks/${id}/state/2`);
-	},
+    axios.put(`/tasks/${id}/state/2`);
+  },
 });
 
 Sortable.create(completed, {
-	group: {
-		name: 'tasks',
-	},
-	animation: 300,
-	chosenClass: 'task-selected',
-	dragClass: 'task-drag',
-	easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
-	store: {
-		set: function (sortable) {
-			const sequence = sortable.toArray();
-			localStorage.setItem('sequence-completed', sequence.join(','));
-		},
+  group: {
+    name: 'tasks',
+  },
+  animation: 300,
+  chosenClass: 'task-selected',
+  dragClass: 'task-drag',
+  easing: 'cubic-bezier(0.5, 1, 0.89, 1)',
+  filter: '#divider',
+  store: {
+    set: function (sortable) {
+      const sequence = sortable.toArray();
+      localStorage.setItem('sequence-completed', sequence.join(','));
+    },
 
-		get: function (sortable) {
-			let sequence = localStorage.getItem('sequence-toDo')
-			sequence = sequence ? sequence.split(',') : {length: 0};
+    get: function (sortable) {
+      let sequence = localStorage.getItem('sequence-toDo');
+      sequence = sequence ? sequence.split(',') : { length: 0 };
 
-			return sequence.length ? sequence : [];
-		},
-	},
+      return sequence.length ? sequence : [];
+    },
+  },
 
-	// save progress
-	onAdd: (event) => {
-		const id = event.item.attributes['data-id'].nodeValue;
+  // save progress
+  onAdd: (event) => {
+    const id = event.item.attributes['data-id'].nodeValue;
 
-		axios.put(`/tasks/${id}/state/3`);
-	},
+    axios.put(`/tasks/${id}/state/3`);
+  },
 });
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,109 +6,117 @@ const { isLoggedIn } = require('../lib/auth');
 const db = require('../db');
 
 router.get('/', isLoggedIn, (req, res) => {
-	const sql = `SELECT * FROM tasks WHERE userID=${req.user.id}`;
+  let sql = `SELECT * FROM tasks WHERE userID=${req.user.id}`;
 
-	db.all(sql, (err, rows) => {
-		if (err) {
-			console.log(err.message);
-			res.status(500).json({ error: err.message });
-		}
+  db.all(sql, (err, tasks) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).json({ error: err.message });
+    }
 
-		const toDo = rows.filter((task) => {
-			return task.state === 1;
-		});
+    sql = `SELECT * FROM dividers WHERE userID=${req.user.id}`;
+    db.all(sql, (err, dividers) => {
+      if (err) {
+        console.log(err.message);
+        res.status(500).json({ error: err.message });
+      }
 
-		const inProgress = rows.filter((task) => {
-			return task.state === 2;
-		});
+      const toDo = tasks.filter((task) => {
+        return task.state === 1;
+      });
 
-		const completed = rows.filter((task) => {
-			return task.state === 3;
-		});
+      const inProgress = tasks.filter((task) => {
+        return task.state === 2;
+      });
 
-		res.render('tasks/list', { toDo, inProgress, completed });
-	});
+      const completed = tasks.filter((task) => {
+        return task.state === 3;
+      });
+
+      res.render('tasks/list', { toDo, inProgress, completed, dividers });
+    });
+  });
 });
 
 router.get('/add', isLoggedIn, (req, res) => {
-	const { state } = req.query;
+  const { state } = req.query;
 
-	res.render('tasks/add', { state });
+  res.render('tasks/add', { state });
 });
 
 router.get('/delete/:id', isLoggedIn, (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	db.run(
-		`DELETE FROM tasks WHERE id=${id} AND userID=${req.user.id}`,
-		(err) => {
-			if (err) {
-				console.log(err.message);
-			}
+  db.run(
+    `DELETE FROM tasks WHERE id=${id} AND userID=${req.user.id}`,
+    (err) => {
+      if (err) {
+        console.log(err.message);
+      }
 
-			res.redirect('/tasks');
-		}
-	);
+      res.redirect('/tasks');
+    }
+  );
 });
 
 router.get('/:id/edit', isLoggedIn, (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const sql = `SELECT * FROM tasks WHERE id=${id} AND userID=${req.user.id}`;
-	db.all(sql, (err, rows) => {
-		if (err) {
-			console.log(err);
-		}
+  const sql = `SELECT * FROM tasks WHERE id=${id} AND userID=${req.user.id}`;
+  db.all(sql, (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
 
-		res.render('tasks/edit', { task: rows[0] });
-	});
+    res.render('tasks/edit', { task: rows[0] });
+  });
 });
 
 router.post('/', isLoggedIn, (req, res) => {
-	const task = req.body;
-	const { state } = req.query;
+  const task = req.body;
+  const { state } = req.query;
 
-	db.run(
-		`INSERT INTO tasks(title, desc, state, userID) VALUES('${task.title}', '${task.desc}', ${state}, ${req.user.id})`,
-		(err) => {
-			if (err) {
-				req.flash('error', 'La tarea no hasido agregada satisfactoriamente.');
-			} else {
-				req.flash('success', 'La tarea ha sido agregada satisfactoriamente.');
-			}
+  db.run(
+    `INSERT INTO tasks(title, desc, state, userID) VALUES('${task.title}', '${task.desc}', ${state}, ${req.user.id})`,
+    (err) => {
+      if (err) {
+        req.flash('error', 'La tarea no hasido agregada satisfactoriamente.');
+      } else {
+        req.flash('success', 'La tarea ha sido agregada satisfactoriamente.');
+      }
 
-			res.redirect('tasks');
-		}
-	);
+      res.redirect('tasks');
+    }
+  );
 });
 
 router.post('/:id/edit', isLoggedIn, (req, res) => {
-	const updatedTask = req.body;
-	const { id } = req.params;
+  const updatedTask = req.body;
+  const { id } = req.params;
 
-	const sql = `UPDATE tasks SET title='${updatedTask.title}', desc='${updatedTask.desc}' WHERE id=${id} AND userID=${req.user.id}`;
-	db.run(sql, (err) => {
-		if (err) {
-			req.flash('error', 'La tarea no ha sido editada satisfactoriamente.');
-		} else {
-			req.flash('success', 'La tarea ha sido editada satisfactoriamente.');
-		}
+  const sql = `UPDATE tasks SET title='${updatedTask.title}', desc='${updatedTask.desc}' WHERE id=${id} AND userID=${req.user.id}`;
+  db.run(sql, (err) => {
+    if (err) {
+      req.flash('error', 'La tarea no ha sido editada satisfactoriamente.');
+    } else {
+      req.flash('success', 'La tarea ha sido editada satisfactoriamente.');
+    }
 
-		res.redirect('/tasks');
-	});
+    res.redirect('/tasks');
+  });
 });
 
 router.put('/:id/state/:state', isLoggedIn, (req, res) => {
-	const { id, state } = req.params;
+  const { id, state } = req.params;
 
-	const sql = `UPDATE tasks SET state=${state} WHERE id=${id} AND userID=${req.user.id}`;
-	db.run(sql, (err) => {
-		if (err) {
-			console.log(err.message);
-			res.sendStatus(500);
-		}
+  const sql = `UPDATE tasks SET state=${state} WHERE id=${id} AND userID=${req.user.id}`;
+  db.run(sql, (err) => {
+    if (err) {
+      console.log(err.message);
+      res.sendStatus(500);
+    }
 
-		res.sendStatus(200);
-	});
+    res.sendStatus(200);
+  });
 });
 module.exports = router;
